9e5951c77e1770e4b1c61912e5de74d2b48cd5d8
Ilyas Sakhanov <ilyas.sakhanov@gmail.com>
2025-06-07 20:47:05 +0200

    Updating README, removing commented code from VPC file

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -53,37 +53,6 @@ resource "aws_default_subnet" "public_subnet3" {
   }
 }
 
-# Subnet for EKS
-# resource "aws_subnet" "private_subnet1" {
-#   vpc_id                  = aws_default_vpc.default.id
-#   cidr_block              = "172.31.48.0/20"
-#   availability_zone       = "us-east-2a"
-#   map_public_ip_on_launch = false
-
-#   tags = {
-#     Name = "private_subnet1"
-#   }  
-# }
-
-# resource "aws_route_table" "private_rt" {
-#   vpc_id = aws_default_vpc.default.id
-
-#   route {
-#     cidr_block = "0.0.0.0/0"
-#     nat_gateway_id = aws_nat_gateway.nat_gateway.id
-#   }
-
-#   tags = {
-#     Name = "private-route-table"
-#   }
-# }
-
-# resource "aws_route_table_association" "private_assoc" {
-#   subnet_id      = aws_subnet.private_subnet1.id
-#   route_table_id = aws_route_table.private_rt.id
-# }
-# EKS stuff ends here
-
 resource "aws_internet_gateway" "gw" {
   vpc_id = aws_default_vpc.default.id
 
